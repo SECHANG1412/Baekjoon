7e0ee9b9bbf0f34996993a295c2d841c96ec1dd6
SECHANG1412 <binaryblitz1412@gmail.com>
2025-03-21 13:36:46 +0900

    [Silver IV] Title: 제로, Time: 76 ms, Memory: 33192 KB -BaekjoonHub

---
+++ Python/백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,16 @@
+import sys
+
+n = int(sys.stdin.readline()) 
+
+m=[]
+
+for i in range(n):
+    num = int(sys.stdin.readline())
+
+    if num==0:
+        m.pop()
+    else:
+        m.append(num)
+
+print(sum(m))
+
