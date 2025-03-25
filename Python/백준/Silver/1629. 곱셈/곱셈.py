94f6acd47e25958d9e5be3243b55a72bb793b6b8
SECHANG1412 <binaryblitz1412@gmail.com>
2025-03-25 23:31:07 +0900

    [Silver I] Title: 곱셈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/1629. 곱셈/곱셈.py
+++ Python/백준/Silver/1629. 곱셈/곱셈.py
@@ -3,15 +3,18 @@ sys.setrecursionlimit(10**6)
 
 A, B, C = map(int, sys.stdin.readline().split())
 
-def power(a, b):
-    if b == 1:
-        return a % C 
-
-    temp = power(a, b // 2)
-
-    if b % 2 == 0:
-        return (temp * temp) % C
+def power(a,b):
+    if b==1:
+        return a%C
+    
+    # 짝수인 경우
+    if b%2 == 0:
+        half=power(a,b//2)
+        return (half*half)%C
+    
+    # 홀수인 경우
     else:
-        return (temp * temp * a) % C
-
-print(power(A, B))
+        half=power(a,b//2)
+        return (half*half*a)%C
+    
+print(power(A,B))
