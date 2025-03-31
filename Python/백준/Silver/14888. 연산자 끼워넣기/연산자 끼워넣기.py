dc273a62a7df6a00403c3af3e1d371062826be37
SECHANG1412 <binaryblitz1412@gmail.com>
2025-03-31 14:36:04 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ Python/백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,39 +1,50 @@
 import sys
 input = sys.stdin.readline
 
-def dfs(index, current):
+def dfs(idx, current):
     global max_result, min_result
 
-    if index == N:
+    # 숫자를 모두 사용-->결과값을 비교해서 최대/최소 저장
+    if idx == N:
         max_result = max(max_result, current)
         min_result = min(min_result, current)
         return
 
     for i in range(4):  
-        if cal_counts[i] > 0:
-            cal_counts[i] -= 1  
-            if i == 0:
-                dfs(index + 1, current + nums[index])
-            elif i == 1:
-                dfs(index + 1, current - nums[index])
-            elif i == 2:
-                dfs(index + 1, current * nums[index])
-            elif i == 3:
-                if current < 0:
-                    dfs(index + 1, -(-current // nums[index]))
-                else:
-                    dfs(index + 1, current // nums[index])
-            cal_counts[i] += 1 
+        if cal[i] > 0:   
+            cal[i] -= 1  
+            
+            # 덧셈
+            if i == 0:   
+                dfs(idx + 1, current + num[idx])
 
+            # 뺄셈
+            elif i == 1: 
+                dfs(idx + 1, current - num[idx])
+
+            # 곱셈
+            elif i == 2: 
+                  dfs(idx + 1, current * num[idx])
+
+            # 나눗셈
+            elif i == 3: 
+                if current < 0:
+                    dfs(idx + 1, -(-current // num[idx])) # 음수일 때
+                else:
+                    dfs(idx + 1, current // num[idx])     # 양수일 때
+
+            cal[i] += 1 
+
+#################################################################
 
 N = int(input())
-nums = list(map(int, input().split()))
-cal_counts = list(map(int, input().split()))  
+num = list(map(int, input().split()))  # 숫자 리스트
+cal = list(map(int, input().split()))  # 연산 리스트
 
-max_result = -float('inf')
-min_result = float('inf')
+max_result = -float('inf')  # 최대값
+min_result = float('inf')   # 최소값
 
-dfs(1, nums[0])
+dfs(1, num[0])
 
 print(max_result)
 print(min_result)
