f0c857df697a647ac3915df0c3379ac5af1679f7
SECHANG1412 <binaryblitz1412@gmail.com>
2025-09-23 15:10:37 +0900

    [Bronze I] Title: 평균, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/1546. 평균/평균.py
@@ -0,0 +1,13 @@
+n = int(input())  
+scores = list(map(int, input().split())) 
+m = max(scores) 
+
+new_scores = []
+
+for score in scores:
+    new_score = (score / m) * 100   
+    new_scores.append(new_score)   
+
+average = sum(new_scores) / n
+
+print(average)
