bfc127eecaf6033ae019fa4877a950e52e200c09
SECHANG1412 <binaryblitz1412@gmail.com>
2025-10-13 13:12:47 +0900

    [Gold V] Title: 신기한 소수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Gold/2023. 신기한 소수/신기한 소수.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+def is_prime(num):
+
+    if num < 2: 
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0: 
+            return False
+    return True
+
+def dfs(num, length):
+    if length == N:
+        print(num)
+        return
+
+    for digit in range(1, 10):
+        new_num = num * 10 + digit   
+        if is_prime(new_num):        
+            dfs(new_num, length + 1) 
+
+N = int(input()) 
+
+for prime in [2, 3, 5, 7]:
+    dfs(prime, 1)
