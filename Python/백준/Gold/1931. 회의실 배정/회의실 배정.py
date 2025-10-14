a382f4e9233c4c8b1a773f0acf12eac4eb66e2de
SECHANG1412 <binaryblitz1412@gmail.com>
2025-10-14 11:16:56 +0900

    [Gold V] Title: 회의실 배정, Time: 236 ms, Memory: 54216 KB -BaekjoonHub

---
+++ Python/백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input()) 
+meetings = []
+
+for _ in range(N):
+    start, end = map(int, input().split())
+    meetings.append((start, end))
+
+meetings.sort(key=lambda x: (x[1], x[0]))
+
+count = 0          
+current_end = 0     
+
+for start, end in meetings:
+    if start >= current_end:  
+        count += 1
+        current_end = end     
+
+print(count)
