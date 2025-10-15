0458b5e607aaa8d20a705cb414bb6e374e446de7
SECHANG1412 <binaryblitz1412@gmail.com>
2025-10-15 10:09:24 +0900

    [Silver I] Title: 최대공약수, Time: 40 ms, Memory: 42176 KB -BaekjoonHub

---
+++ Python/백준/Silver/1850. 최대공약수/최대공약수.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+a, b = map(int, input().split())
+
+def gcd(x, y):
+    while y != 0:
+        x, y = y, x % y
+    return x
+
+g = gcd(a, b)
+
+print('1' * g)
