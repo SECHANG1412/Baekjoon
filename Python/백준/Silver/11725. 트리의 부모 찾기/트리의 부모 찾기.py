56e4050e919a199aa8561dd29ccd402047e05c67
SECHANG1412 <binaryblitz1412@gmail.com>
2025-10-18 07:22:08 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 264 ms, Memory: 51144 KB -BaekjoonHub

--- Python/백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ Python/백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,38 +1,35 @@
 import sys
 sys.setrecursionlimit(10**6)
-input = sys.stdin.readline
+input=sys.stdin.readline
 
-# 스택
 def dfs(start):
-    stack = [start]
+    stack=[start]
 
     while stack:
-        node = stack.pop()
+        node=stack.pop()
 
         if not visited[node]:
-            visited[node] = True
+            visited[node]=True
 
-            for next_node in graph[node]:
-                if not visited[next_node]:
-                    parent[next_node] = node
-                    stack.append(next_node)
+        for next_node in graph[node]:
+            if not visited[next_node]:
+                parent[next_node]=node
+                stack.append(next_node)
 
-############################################################################
+###############################################
+N=int(input().strip())
 
-N = int(input().strip())
+graph=[[] for _ in range(N+1)]
 
-graph = [[] for _ in range(N + 1)]
-
-for _ in range(N - 1):
-    a, b = map(int, input().split())
+for _ in range(N-1):
+    a,b=map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-parent = [0] * (N + 1) 
-visited = [False] * (N + 1)
+parent=[0]*(N+1)
+visited=[False]*(N+1)
 
 dfs(1)
 
-# 2번 노드부터 N번 노드까지 부모 노드를 출력
-for i in range(2, N + 1):
+for i in range(2,N+1):
     print(parent[i])
